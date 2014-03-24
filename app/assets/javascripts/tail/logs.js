212c75a793e9b588d722cceefe488ac86ae58f03
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-03-24 17:33:26 +0300

    loading on scroll up created, but not implemented

--- app/assets/javascripts/tail/logs.js
+++ app/assets/javascripts/tail/logs.js
@@ -1,14 +1,54 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        initApplication();
+    }
+}
+
+function initApplication(){
+    var appended = false, up_arrow = document.querySelector("#up");
+
+    onscroll = function () {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (scrollTop < -5) {
+            console.log('scrolled', scrollTop, appended);
+            var old_value = parseInt(getPageVar('n'));
+            var new_value = (old_value * 1.1).toFixed();
+
+            var query = location.pathname + 'grep' + location.search.replace('n=' + old_value, 'n=' + new_value);
+//            sendRequest(query, false)
+            console.log('scrolled', query);
+        }
+        if (scrollTop > 500) {
+            if (!appended) {
+                up_arrow.hidden = false;
+                appended = true;
+            }
+        } else {
+            if (appended) {
+                up_arrow.hidden = true;
+                appended = false;
+            }
+        }
+    };
+
+}
+
+function getPageVar(sVar) {
+    return decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+}
+
 function getGrep(text) {
     var sign;
     if (location.search == "") sign = "?"; else sign = "&";
     var search = sign + 'query=' + text;
     var query = location.pathname + 'grep' + location.search + search;
-    sendRequest(query)
+    sendRequest(query, true)
 }
 
-function sendRequest(url) {
+function sendRequest(url, isGoToBottom) {
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
         httpRequest = new XMLHttpRequest();
     } else if (window.ActiveXObject) { // IE
@@ -29,9 +69,10 @@ function sendRequest(url) {
     httpRequest.open("GET", url);
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState != 4 || httpRequest.status != 200) return;
-//        console.log("Success: " + httpRequest.responseText);
         document.getElementById('main').innerHTML = httpRequest.responseText;
-        window.scrollTo(0, document.body.scrollHeight);
+        if (isGoToBottom) {
+            window.scrollTo(0, document.body.scrollHeight);
+        }
     };
     httpRequest.send();
     return true;
