891d86bd55134f34335cf6e3dd844d73c6665246
Aleksey Ivanov <ialexxei@gmail.com>
2016-08-23 21:20:53 +0300

    add assets precompilation

--- lib/tail/engine.rb
+++ lib/tail/engine.rb
@@ -1,5 +1,9 @@
 module Tail
   class Engine < ::Rails::Engine
     isolate_namespace Tail
+
+    initializer 'tail.assets.precompile' do |app|
+      app.config.assets.precompile += %w(tail/refresh.png tail/cleaning.png tail/up_arrow.png)
+    end
   end
 end
